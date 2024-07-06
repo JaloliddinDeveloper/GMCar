71d4ff3ed0086d64b0678a63591f571d629fca03
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-07-07 00:16:30 +0500

    Add project files.

---
+++ GMCar/Classes/Car.cs
@@ -0,0 +1,33 @@
+﻿namespace GMCar.Classes
+{
+    public class Car
+    {
+        private string Model { get; set; }
+        private int Year { get; set; }
+        private decimal Price { get; set; }
+        private decimal Speed { get; set; }
+
+        public Car(string model, int year, decimal price, decimal speed)
+        {
+            this.Model = model;
+            this.Year = year;
+            this.Price = price;
+            this.Speed = speed;
+        }
+
+        public string GetCarInfo() =>
+             $"Medel is {Model}, Year is {Year}, Price is {Price}, Speed is {Speed}";
+
+        public decimal CalculateDepreciation(int years)
+        {
+            if (years <= 10)
+            {
+                return Price - Price * years / 10;
+            }
+            else 
+                return 0;
+        }
+    }
+}
+        
+       
---
+++ GMCar/Program.cs
@@ -0,0 +1,22 @@
+﻿using GMCar.Classes;
+
+var Damas = new Car(model: "Damas", year: 2003, price: 10000000, speed: 180);
+var Nexia = new Car(model: "Nexia", year: 2004, price: 20000000, speed: 200);
+var Lasetti = new Car(model: "Lasetti", year: 2006, price: 25000000, speed: 220);
+
+Console.WriteLine(Damas.GetCarInfo());
+Console.WriteLine(Nexia.GetCarInfo());
+Console.WriteLine(Lasetti.GetCarInfo());
+
+Console.WriteLine();
+
+Console.WriteLine($"Damasning chiqarilgandan 3 yildan keyingi narxi:" +
+    $" {Damas.CalculateDepreciation(3)} ");
+
+Console.WriteLine($"Nexianing chiqarilgandan 5 yildan keyingi narxi: " +
+    $"{Nexia.CalculateDepreciation(5)} ");
+
+Console.WriteLine($"Lasettining chiqarilgandan 7 yildan keyingi narxi:" +
+    $" {Lasetti.CalculateDepreciation(7)} ");
+
+
